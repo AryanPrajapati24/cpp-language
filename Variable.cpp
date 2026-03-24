e17090bb7c11cd4ef755dac5021240e5d2355ef8
aaryannprajapatii24-tech <aaryannprajapatii24@gmail.com>
2026-03-24 15:12:18 +0530

    Initial commit

---
+++ Variable.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int studantid;
+    string studantname;
+    float persantage;
+    string grade;
+
+    cout<<"Enter a studantID:";
+    cin>>studantid;
+
+    cout<<"Enter a studantname:";
+    cin>>studantname;
+
+    cout<<"Enter a persantage:";
+    cin>>persantage;
+
+    cout<<"Enter a grade:";
+    cin>>grade;
+
+    cout<<"\n\n||-------Studant Info-----||"<<endl;
+    cout<<"studantid:"<<studantid<<endl;
+    cout<<"studantname:"<<studantname<<endl;
+    cout<<"persantage:"<<persantage<<endl;
+    cout<<"grade:"<<grade<<endl;
+
+
+    return 0;
+
+
+}
