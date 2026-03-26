e234560b2d9211eb5a2668a9cef0c7466841bd76
aaryannprajapatii24-tech <aaryannprajapatii24@gmail.com>
2026-03-26 17:28:07 +0530

    make a class

---
+++ 5-class.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+class Hello{
+    public:
+        void greet(){
+            cout<<"Hey Developers.!!";
+        }
+        void sayHello(){
+            cout<<"Hello Everyone.!"<<endl;
+        }
+};
+int main(){
+    Hello h;
+    h.greet();
+    h.sayHello();
+    return 0;
+}
